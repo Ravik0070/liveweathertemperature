07884e2e3d8bbd36dd0e19a1cf92a929e5e12069
Ravikant <kant81272@gmail.com>
2022-01-26 22:47:11 +0530

    change

--- weatherapi/urls.py
+++ weatherapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from weatherapi.views import home,searchcity
+from weatherapi.views import home,searchcity,livehome
 
 app_name = 'weatherapi'
 
 urlpatterns = [
     path('',home,name="home"),
+    path('livetemp/',livehome,name="livehome"),
     path('searchcity/',searchcity,name="searchcity"),
 ]
--- weatherapi/views.py
+++ weatherapi/views.py
@@ -2,26 +2,18 @@ from django.shortcuts import redirect, render
 import requests
 from django.conf import settings
 import datetime
-import geocoder
-
+from geopy.geocoders import Nominatim
 
 def home(request):
      # -------------location------
         api = settings.API_KEY
         res = requests.get('https://ipinfo.io/')
         data = res.json()
+        lat1 , long1  = data['loc'].split(',',1)
+        lat = float(lat1)
+        lon = float(long1)
         city = data['city']
         country = data['country']
-        region = data['region']
-        timezones = data['timezone']
-        g = geocoder.ip('me')
-        l1 = g.latlng[0]
-        l2 = g.latlng[1]
-        lat1 , long1  = data['loc'].split(',',1)
-        # lat = float(lat1)
-        lat = float(l1)
-        # lon = float(long1)
-        lon = float(l2)
 
         # -------------location end------
 
@@ -30,7 +22,6 @@ def home(request):
         temp = response['main']['temp']
         temp = kelvinToCelcius(temp)
         desc = response['weather'][0]['description']
-        city = data['city']
         humidity = response['main']['humidity']
 
         # livecontext = {'temp':temp,'desc':desc,'city':city,'country':country,'region':region}
@@ -55,7 +46,57 @@ def home(request):
                 forecastDate[i] = forecastDate[i].strftime('%d %b, %Y')
 
         return render(request,'base.html',{'fdate':forecastDate,'ftemp':forecastTemp,'fwind':forecastWind,'fdesc':forecastDesc
-        ,'temp':temp,'desc':desc,'city':city,'country':country,'region':region,'humidity':humidity,'date':d})
+        ,'temp':temp,'desc':desc,'humidity':humidity,'date':d,'city':city,'country':country})
+
+
+def livehome(request):
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        api = settings.API_KEY
+        if request.method == "GET":
+                l1 = request.GET.get('latitude','28.6504')  
+                l2 = request.GET.get('longitude','77.2372') 
+                print(l1)
+                lat = float(l1)
+                lon = float(l2)
+                city = str(geolocator.reverse(f'{lat},{lon}'))
+                city_list = city.split(',')
+                city = city_list[0]
+                city1 = city_list[3]
+                url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}'
+                response = requests.get(url).json()
+                temp = response['main']['temp']
+                temp = kelvinToCelcius(temp)
+                desc = response['weather'][0]['description']
+                humidity = response['main']['humidity']
+
+        # livecontext = {'temp':temp,'desc':desc,'city':city,'country':country,'region':region}
+                forecastDate = [] 
+                forecastTemp = [] 
+                forecastWind = []
+                forecastDesc = []
+
+                url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly&appid={api}'
+                response = requests.get(url).json()
+                t = response['daily'] 
+                d = datetime.datetime.fromtimestamp(t[0]['dt'])
+                d = d.strftime('%d %b,%Y')
+                for i in range(5):
+                        forecastDate.append(datetime.datetime.fromtimestamp(t[i]['dt']))
+                        forecastTemp.append(kelvinToCelcius(temp = t[i]['temp']['day']))
+                        forecastWind.append(t[i]['wind_speed']) #m/s
+                        forecastDesc.append(t[i]['weather'][0]['description'])
+        # forecast = {'fdate':forecastDate,'ftemp':forecastTemp,'fwind':forecastWind,'fdesc':forecastDesc}
+        
+                for i in range(len(forecastDate)):
+                        forecastDate[i] = forecastDate[i].strftime('%d %b, %Y')
+
+                return render(request,'livelocation.html',{'fdate':forecastDate,'ftemp':forecastTemp,'fwind':forecastWind,'fdesc':forecastDesc
+                ,'temp':temp,'desc':desc,'city':city,'city1':city1,'humidity':humidity,'date':d})
+        else:
+                return render(request,'result.html')
+
+
+
 
 def searchcity(request):
         api = settings.API_KEY
